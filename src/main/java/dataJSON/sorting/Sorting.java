99180796d70cbe1302a30eaa9cc7ede5042a5ee8
Ramazan <ramazan_sanlioglu@live.com>
2021-02-24 10:33:31 +0100

    Created interface for sorting and a class that implements this sorting

---
+++ src/main/java/dataJSON/sorting/Sorting.java
@@ -0,0 +1,34 @@
+package dataJSON.sorting;
+
+public class Sorting implements ISorting{
+
+    @Override
+    public void sortASCint() {
+
+    }
+
+    @Override
+    public void sortASCdouble() {
+
+    }
+
+    @Override
+    public void sortASCfloat() {
+
+    }
+
+    @Override
+    public void sortDESCint() {
+
+    }
+
+    @Override
+    public void sortDESCdouble() {
+
+    }
+
+    @Override
+    public void sortDESCfloat() {
+
+    }
+}
