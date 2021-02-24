5e112a6d61340b21e848ec45d709184630fe2ac5
Ramazan <ramazan_sanlioglu@live.com>
2021-02-24 10:38:10 +0100

    Created interface for calculation and a class that implements it

---
+++ src/main/java/dataJSON/calculation/ICalculate.java
@@ -0,0 +1,9 @@
+package dataJSON.calculation;
+
+public interface ICalculate {
+    double calcAvg();
+    int occurrence();
+    int findMaxInt(); //Flere konstruktører for ulike datatyper?
+    Double findMaxDouble(); //Flere konstruktører for ulike datatyper?
+    Float findMaxFloat(); //Flere konstruktører for ulike datatyper?
+}
