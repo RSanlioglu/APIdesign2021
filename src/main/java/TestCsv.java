0130cd44874a5b577e702affecaacdd3286a5da0
Kabaciok <61049550+ViolaSam@users.noreply.github.com>
2021-03-08 12:36:22 +0100

    Deleted some files and added interfaces.

--- src/main/java/TestCsv.java
+++ src/main/java/TestCsv.java
@@ -1,4 +1,5 @@
 import dataCSV.dataAcces.DataAccess;
+import dataCSV.dataAcces.Runner;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -7,6 +8,14 @@ import java.util.Collections;
 public class TestCsv {
 
     public static void main(String[] args) {
+        DataAccess csv = new DataAccess();
+
+        ArrayList<Runner> runner = new ArrayList <>();
+        runner.add(new Runner("01","Roberts", "Juarez",21, 'M',"Ethiopia",9.5f,
+                10, 3, 0));
+
+        csv.writeCollectionToDataFile(Collections.singletonList(runner),"runner.csv", Runner.class,
+                false);
 
     }
 }
