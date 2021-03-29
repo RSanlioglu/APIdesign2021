b47eb2887bd838a4642750709d60762b60873141
Ramazan <ramazan_sanlioglu@live.com>
2021-03-29 18:44:26 +0200

    Changed the structure of the DataAccess and implemented it

--- src/main/java/dataCSV/dataAcces/IDataAccess.java
+++ src/main/java/dataCSV/dataAcces/IDataAccess.java
@@ -1,5 +1,15 @@
 package dataCSV.dataAcces;
 
+import java.util.List;
+
 public interface IDataAccess {
-    void createCsv();
+    void createCSV();                                       //Create an empty csv file
+    List<Object> getAllObjects();                           //Return all of the objects from csv file
+    void writeObject(Object o);                             //Override one object to the file
+    void writeList(List<Object> l_o);                       //Override one list to the file
+    void appendObject(Object o);                            //Add one object to the file
+    void appendList(List<Object> l_o);                      //Add one list of objects to the file
+    boolean doesExist(Object o);                            //Check if object exists in file
+    void deleteObject(Object o);                            //Delete an object from csv file
+    void updateObject(Object oldObject, Object newObject);  //Update an object (Delete old object, add one new object)
 }
