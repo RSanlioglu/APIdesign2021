692aa74c4fb138509ae6c88d7f7335239bb0d08f
Ramazan <ramazan_sanlioglu@live.com>
2021-03-30 11:54:40 +0200

    Removed TODO from testCSV.java

--- src/main/java/TestCsv.java
+++ src/main/java/TestCsv.java
@@ -7,8 +7,6 @@ import java.util.Collections;
 public class TestCsv {
 
     public static void main(String[] args) {
-        //TODO: Create exception for existing file
-        //TODO: Hør med lærer om vi må duplisere testene som jeg har gjort
     }
 }
 
