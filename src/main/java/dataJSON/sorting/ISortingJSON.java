8d4397876dd21a5bf072ccf8aa4dcb6e08a56e51
Ramazan <ramazan_sanlioglu@live.com>
2021-03-31 21:45:43 +0200

    Created a simple sorting-method. NOT finished and will be more

--- src/main/java/dataJSON/sorting/ISortingJSON.java
+++ src/main/java/dataJSON/sorting/ISortingJSON.java
@@ -1,11 +1,5 @@
 package dataJSON.sorting;
 
 public interface ISortingJSON {
-    void sortASCint();
-    void sortASCdouble();
-    void sortASCfloat();
-    void sortDESCint();
-    void sortDESCdouble();
-    void sortDESCfloat();
 
 }
--- src/main/java/dataJSON/sorting/SortingJSON.java
+++ src/main/java/dataJSON/sorting/SortingJSON.java
@@ -1,34 +1,51 @@
 package dataJSON.sorting;
 
+import java.util.*;
+
 public class SortingJSON implements ISortingJSON {
 
-    @Override
-    public void sortASCint() {
+    public SortingJSON() {
 
     }
 
-    @Override
-    public void sortASCdouble() {
+    /**
+     * The client sends in a hashmap of data. The key values will be sorted
+     * with a merge-sort algorithm and a list of sorted objects is then returned
+     * to the client. Note! Changes are not printed on datafile until client
+     * writes it on there using the DataAccess.
+     *
+     * @param mapOfObjects - A hashmap containing objects and the keys to the objects
+     * @return - A list of objects that are sorted by the key integer values (Ascending)
+     */
 
+    public List<? extends Object> sortIntASC(HashMap<Integer, ?> mapOfObjects) {
+        List<Object> sortedList = new ArrayList<>();
+
+        List<Integer> intToBeSorted = new ArrayList<>(mapOfObjects.keySet());
+
+        for(Integer i : sort(intToBeSorted)) {
+            sortedList.add(mapOfObjects.get(i));
+        }
+
+        return sortedList;
     }
 
-    @Override
-    public void sortASCfloat() {
+    private List<Integer> sort(List<Integer> integerList) {
+        int n = integerList.size();
+        int min, tmp;
 
-    }
+        for(int i = 0; i < n-1; i++) {
+            min = i;
+            for(int j = i+1; j < n; j++) {
+                if(integerList.get(j) < integerList.get(min)) {
+                    min = j;
+                }
+            }
+            tmp = integerList.get(min);
+            integerList.set(min, integerList.get(i));
+            integerList.set(i, tmp);
+        }
 
-    @Override
-    public void sortDESCint() {
-
-    }
-
-    @Override
-    public void sortDESCdouble() {
-
-    }
-
-    @Override
-    public void sortDESCfloat() {
-
+        return integerList;
     }
 }
