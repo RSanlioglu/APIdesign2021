e0550aea4662e164906204d3909b4a1e16bedbd9
Ramazan <ramazan_sanlioglu@live.com>
2021-04-05 12:48:32 +0200

    Created a sort function for sorting integers descending. Modified the algorithm to also sort double insted of just ints.

--- src/main/java/dataJSON/sorting/SortingAlgorithm.java
+++ src/main/java/dataJSON/sorting/SortingAlgorithm.java
@@ -6,7 +6,7 @@ public abstract class SortingAlgorithm {
      * The algorithm is obtained from the course ITF20006-1 21V Algorithms and datastructures
      * and modified for the use of this framework
      * @param intToBeSorted - An array of integers that will be sorted (the core data for sorting)
-     * @param first - The index for the first value (usually 0) //TODO: Consider to change this to 0
+     * @param first - The index for the first value (usually 0)
      * @param last - The index for the last value.
      */
     public static void mergeSortINT(int[] intToBeSorted, int first, int last) {
@@ -46,4 +46,51 @@ public abstract class SortingAlgorithm {
             }
         }
     }
+
+    /**
+     * A simple Merge Sort algorithm used to sort data.
+     * The algorithm is obtained from the course ITF20006-1 21V Algorithms and datastructures
+     * and modified for the use of this framework
+     * @param doubleToBeSorted - An array of doubles that will be sorted (the core data for sorting)
+     * @param first  The index for the first value (usually 0)
+     * @param last - The index for the last value
+     */
+    public static void mergeSortDouble(double[] doubleToBeSorted, int first, int last) {
+        if(first==last) {
+            return;
+        }
+
+        double[] temp;
+        int i, left, right;
+        int size = last - first + 1;
+        int mid = (first + last) / 2;
+
+        temp = new double[size];
+
+        mergeSortDouble(doubleToBeSorted, first, mid);
+        mergeSortDouble(doubleToBeSorted, mid+1, last);
+
+        for(i = 0; i < size; i++) {
+            temp[i] = doubleToBeSorted[first + i];
+        }
+
+        left = 0;
+        right = mid - first + 1;
+        for(i = 0; i < size; i++) {
+            if(right <= last - first) {
+                if(left <= mid - first) {
+                    if(temp[left] > temp[right]) {
+                        doubleToBeSorted[i + first] = temp[right++];
+                    } else {
+                        doubleToBeSorted[i + first] = temp[left++];
+                    }
+                } else {
+                    doubleToBeSorted[i + first] = temp[right++];
+                }
+            } else {
+                doubleToBeSorted[i + first] = temp[left++];
+            }
+        }
+    }
+
 }
--- src/main/java/dataJSON/sorting/SortingJSON.java
+++ src/main/java/dataJSON/sorting/SortingJSON.java
@@ -17,7 +17,6 @@ public class SortingJSON implements ISortingJSON {
      * @param mapOfObjects - A hashmap containing objects and the keys to the objects
      * @return - A list of objects that are sorted by the key integer values (Ascending)
      */
-
     public List<? extends Object> sortIntASC(HashMap<Integer, ?> mapOfObjects) {
         List<Object> sortedList = new ArrayList<>();
 
@@ -30,6 +29,33 @@ public class SortingJSON implements ISortingJSON {
         SortingAlgorithm.mergeSortINT(intToBeSorted, 0, intToBeSorted.length - 1);
 
         for(int i : intToBeSorted) {
+            sortedList.add(mapOfObjects.get(i));
+        }
+
+        return sortedList;
+    }
+
+    /**
+     * The client sends in a hashmap of data. The key values will be sorted where the int value is descending,
+     * with a merge-sort algorithm and a list of sorted objects is then returned
+     * to the client. Note! Changes are not printed on datafile until client
+     * writes it on there using the DataAccess.
+     *
+     * @param mapOfObjects - A hashmap containing objects and the keys to the objects
+     * @return - A list of objects that are sorted by the key integer values (Ascending)
+     */
+    public List<? extends Object> sortIntDESC(HashMap<Integer, ?> mapOfObjects) {
+        List<Object> sortedList = new ArrayList<>();
+
+        int[] intToBeSorted = new int[mapOfObjects.size()];
+        int j = 0;
+        for(Integer i : mapOfObjects.keySet()) {
+            intToBeSorted[j++] = i;
+        }
+
+        SortingAlgorithm.mergeSortINT(intToBeSorted, 0, intToBeSorted.length - 1 );
+
+        for(int i = mapOfObjects.size() + 1; i-- > 1;) {
             sortedList.add(mapOfObjects.get(i));
         }
 
