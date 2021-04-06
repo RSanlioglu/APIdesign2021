f2fb58ac8d6efea94b4a9a63146f66aa2dc07932
Ramazan <ramazan_sanlioglu@live.com>
2021-04-06 19:28:43 +0200

    Fixed ISortingJSON

--- src/main/java/dataJSON/sorting/ISortingJSON.java
+++ src/main/java/dataJSON/sorting/ISortingJSON.java
@@ -1,5 +1,13 @@
 package dataJSON.sorting;
 
-public interface ISortingJSON {
+import java.util.HashMap;
+import java.util.List;
 
+public interface ISortingJSON {
+    List<? extends Object> sortIntASC(HashMap<Integer, ?> mapOfObjects);
+    List<? extends Object> sortIntDESC(HashMap<Integer, ?> mapOfObjects);
+    List<? extends Object> sortDoubleASC(HashMap<Double, ?> mapOfObjects);
+    List<? extends Object> sortDoubleDESC(HashMap<Double, ?> mapOfObjects);
+    List<? extends Object> sortStringAlphabeticalASC(HashMap<String, ?> mapOfObjects);
+    List<? extends Object> sortStringAlphabeticalDESC(HashMap<String, ?> mapOfObjects);
 }
