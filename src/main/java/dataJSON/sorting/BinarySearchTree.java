64422d71355f82f055e530e955c92f129d40b5b4
Ramazan <ramazan_sanlioglu@live.com>
2021-04-06 10:46:40 +0200

    Added some comments to BinarySearchTree.java

--- src/main/java/dataJSON/sorting/BinarySearchTree.java
+++ src/main/java/dataJSON/sorting/BinarySearchTree.java
@@ -1,29 +1,36 @@
 package dataJSON.sorting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinarySearchTree {
     private TreeNode root;
 
+    //Constructor that creates a search tree with a null value for the root
     public BinarySearchTree() {
         root = null;
     }
 
-    public boolean isEmpty() {
-        return root == null;
-    }
-
+    /**
+     * The words are inserted into the tree and will be easy to
+     * traverse through in order to sort the strings in an alphabetical order
+     * @param value - String that is to be inserted into the tree
+     */
     public void insert(String value) {
         TreeNode newNode = new TreeNode(value);
 
-        if (isEmpty()) {
+        //See if the tree is empty
+        if (root == null) {
             root = newNode;
             return;
         }
+
         TreeNode current = root;
         boolean finished = false;
 
         while (!finished) {
             int comp = value.compareTo(String.valueOf(current.data));
-
+            //Comparing the strings
             if (comp < 0) {
                 if (current.left == null) {
                     current.left = newNode;
