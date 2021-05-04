88f23815d83adf4cae0f904b5506da84c163bb71
Ramazan <ramazan_sanlioglu@live.com>
2021-05-04 17:08:56 +0200

    Interface for calculation is updated and comments are added

--- src/main/java/Calculation/Calculation.java
+++ src/main/java/Calculation/Calculation.java
@@ -7,18 +7,30 @@ import DataAccess.DataAccessXML;
 import java.util.List;
 
 /**
- * Factory for creating calculation instances for CSV files.
- * This class implements the interface ICalculation.
+ * Factory for creating a Calculation instance. The factory contains
+ * three overloads for its constructor, supporting three datafiles (CSV, XML and JSON)
  */
-public class Calculation {
+public class Calculation implements ICalculation{
+    /**
+     * Enum containing three operations such as SUM, MIN and MAX.
+     * Making it easier for client to calculate a column
+     */
     public enum Method {
         SUM,
         MIN,
         MAX
     }
 
+    /**
+     * List of objects used for calculation
+     */
     private static List<Object> objects;
 
+    /**
+     * Constructor for Calculation, supporting XML data-access
+     * @param xml - Data-access for the data to be calculated
+     * @throws NullPointerException - When data access is a nullpointer
+     */
     public Calculation(DataAccessXML xml) throws NullPointerException {
         if(xml == null) {
             throw new NullPointerException();
@@ -28,6 +40,11 @@ public class Calculation {
 
     }
 
+    /**
+     * Constructor for Calculation, supporting JSON data-access
+     * @param json - Data-access for the data to be calculated
+     * @throws NullPointerException - When data access is a nullpointer
+     */
     public Calculation(DataAccessJSON json) throws NullPointerException {
         if(json == null) {
             throw new NullPointerException();
@@ -36,7 +53,11 @@ public class Calculation {
         }
     }
 
-
+    /**
+     * Constructor for Calculation, supporting CSV data-access
+     * @param csv - Data-access for the data to be calculated
+     * @throws NullPointerException - When data access is a nullpointer
+     */
     public Calculation(DataAccessCSV csv) throws NullPointerException {
         if(csv == null) {
             throw new NullPointerException();
@@ -45,6 +66,14 @@ public class Calculation {
         }
     }
 
+    /**
+     * Function for calculating a columns MAX value, MIN value and SUM value
+     * @param columnName - Name of column you want to execute calculation operation on
+     * @param type - Type of class that the objects consist of
+     * @param operation - The operation that the client wishes to use (MIN, MAX and SUM)
+     * @return integer value of the calculations
+     */
+    @Override
     public int calculateColumnInt(String columnName, Class<?> type, Method operation) {
         switch (operation) {
             case MAX:
@@ -57,6 +86,14 @@ public class Calculation {
         return -1;
     }
 
+    /**
+     * Function for calculating a columns MAX value, MIN value and SUM value
+     * @param columnName - Name of column you want to execute calculation operation on
+     * @param type - Type of class that the objects consist of
+     * @param operation - The operation that the client wishes to use (MIN, MAX and SUM)
+     * @return double value of the calculations
+     */
+    @Override
     public double calculateColumnDouble(String columnName, Class<?> type, Method operation) {
         switch (operation) {
             case MAX:
@@ -69,22 +106,62 @@ public class Calculation {
         return -1;
     }
 
+    /**
+     * Function for calculating the average int of a column.
+     * @param columnName - Name of the column you want to find the average of
+     * @param type - Type of class that the objects consist of
+     * @return - average value
+     */
+    @Override
     public double calculateColumnAverageInt(String columnName, Class<?> type) {
         return Operations.calculateColumnAverageInt(objects, columnName, type);
     }
 
+    /**
+     * Function for calculating the average double of a column.
+     * @param columnName - Name of the column you want to find the average of
+     * @param type - Type of class that the objects consist of
+     * @return - average value
+     */
+    @Override
     public double calculateColumnAverageDouble(String columnName, Class<?> type) {
         return Operations.calculateColumnAverageDouble(objects, columnName, type);
     }
 
+    /**
+     * Function for counting how many times an int reference value is found inside the datafile.
+     * @param columnName - Name of the column you want to search through
+     * @param type - Type of class that the objects consist of
+     * @param refValue - Reference-value used for checking
+     * @return - How many times a value is found in the data-file
+     */
+    @Override
     public int countIntInColumn(String columnName, Class<?> type, int refValue) {
         return Operations.countIntValue(objects, columnName, type, refValue);
     }
 
+
+    /**
+     * Function for counting how many times a double reference value is found inside the datafile.
+     * @param columnName - Name of the column you want to search through
+     * @param type - Type of class that the objects consist of
+     * @param refValue - Reference-value used for checking
+     * @return - How many times a value is found in the data-file
+     */
+    @Override
     public int countDoubleInColumn(String columnName, Class<?> type, double refValue) {
         return Operations.countDoubleValue(objects, columnName, type, refValue);
     }
 
+
+    /**
+     * Function for counting how many times a string reference value is found inside the datafile.
+     * @param columnName - Name of the column you want to search through
+     * @param type - Type of class that the objects consist of
+     * @param refValue - Reference-value used for checking
+     * @return - How many times a value is found in the data-file
+     */
+    @Override
     public int countStringInColumn(String columnName, Class<?> type, String refValue) {
         return Operations.countStringValue(objects, columnName, type, refValue);
     }
--- src/main/java/Calculation/ICalculation.java
+++ src/main/java/Calculation/ICalculation.java
@@ -1,15 +1,11 @@
 package Calculation;
 
 public interface ICalculation {
-    int calculateColumnSumInt(String columnName, Class<?> type);
-    double calculateColumnSumDouble(String columnName, Class<?> type);
+    int calculateColumnInt(String columnName, Class<?> type, Calculation.Calculation.Method operation);
+    double calculateColumnDouble(String columnName, Class<?> type, Calculation.Calculation.Method operation);
     double calculateColumnAverageInt(String columnName, Class<?> type);
     double calculateColumnAverageDouble(String columnName, Class<?> type);
-    int calculateColumnMinInt(String columnName, Class<?> type);
-    int calculateColumnMaxInt(String columnName, Class<?> type);
-    double calculateColumnMinDouble(String columnName, Class<?> type);
-    double calculateColumnMaxDouble(String columnName, Class<?> type);
-    int countIntValue(String columnName, Class<?> type, int refValue);
-    int countStringValue(String columnName, Class<?> type, String refValue);
-    int countDoubleValue(String columnName, Class<?> type, double refValue);
+    int countIntInColumn(String columnName, Class<?> type, int refValue);
+    int countDoubleInColumn(String columnName, Class<?> type, double refValue);
+    int countStringInColumn(String columnName, Class<?> type, String refValue);
 }
